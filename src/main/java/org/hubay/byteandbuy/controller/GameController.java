e27588a8b7a3cdb945de34bfa9aba80e9504a548
ladislavHubay <hubayladislav7@gmail.com>
2026-04-18 20:03:43 +0200

    refactoring

--- src/main/java/org/hubay/byteandbuy/controller/GameController.java
+++ src/main/java/org/hubay/byteandbuy/controller/GameController.java
@@ -4,10 +4,7 @@ import org.hubay.byteandbuy.dto.TurnResponse;
 import org.hubay.byteandbuy.model.game.Game;
 import org.hubay.byteandbuy.service.GameEngine;
 import org.hubay.byteandbuy.service.GameService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/game")
@@ -21,42 +18,48 @@ public class GameController {
     }
 
     // Spustenie jedného ťahu
-    // http://localhost:8080/game/turn
-    @PostMapping("/turn")
-    public TurnResponse playTurn() {
-        Game game = gameService.getGame();
+    // http://localhost:8080/game/{gameId}/turn
+    @PostMapping("/{gameId}/turn")
+    public TurnResponse playTurn(@PathVariable String gameId) {
+        Game game = gameService.getGame(gameId);
         return engine.playTurn(game);
     }
 
     // spusti nakup policka na ktorom hrac stoji
-    // http://localhost:8080/game/buy
-    @PostMapping("/buy")
-    public TurnResponse buyProperty() {
-        Game game = gameService.getGame();
+    // http://localhost:8080/game/{gameId}/buy
+    @PostMapping("/{gameId}/buy")
+    public TurnResponse buyProperty(@PathVariable String gameId) {
+        Game game = gameService.getGame(gameId);
         return engine.buyProperty(game);
     }
 
     // Volba - nekupit policko
-    // http://localhost:8080/game/skip
-    @PostMapping("/skip")
-    public TurnResponse skip() {
-        Game game = gameService.getGame();
+    // http://localhost:8080/game/{gameId}/skip
+    @PostMapping("/{gameId}/skip")
+    public TurnResponse skip(@PathVariable String gameId) {
+        Game game = gameService.getGame(gameId);
         return engine.skipPurchase(game);
     }
 
     // Hrac ukonci svoju hru. Ostatny hraci hraju dalej.
-    // http://localhost:8080/game/leave
-    @PostMapping("/leave")
-    public Game leaveGame() {
-        Game game = gameService.getGame();
+    // http://localhost:8080/game/{gameId}/leave
+    @PostMapping("/{gameId}/leave")
+    public Game leaveGame(@PathVariable String gameId) {
+        Game game = gameService.getGame(gameId);
         engine.leaveGame(game);
         return game;
     }
 
     // Získať aktuálny stav hry (bez zmeny)
-    // http://localhost:8080/game/state
-    @GetMapping("/state")
-    public Game getState() {
-        return gameService.getGame();
+    // http://localhost:8080/game/{gameId}/state
+    @GetMapping("/{gameId}/state")
+    public Game getState(@PathVariable String gameId) {
+        return gameService.getGame(gameId);
+    }
+
+    // http://localhost:8080/game/create
+    @PostMapping("/create")
+    public String createGame() {
+        return gameService.createGame();
     }
 }
--- src/main/java/org/hubay/byteandbuy/service/GameService.java
+++ src/main/java/org/hubay/byteandbuy/service/GameService.java
@@ -5,12 +5,33 @@ import org.hubay.byteandbuy.factory.GameFactory;
 import org.hubay.byteandbuy.model.game.Game;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @Getter
 @Service
 public class GameService {
-    private final Game game;
+    private final GameFactory gameFactory;
+    private final Map<String, Game> games = new HashMap<>();
 
-    public GameService(GameFactory factory) {
-        this.game = factory.createSampleGame();
+    public GameService(GameFactory gameFactory) {
+        this.gameFactory = gameFactory;
+    }
+
+    public String createGame() {
+        String gameId = UUID.randomUUID().toString();
+        games.put(gameId, gameFactory.createSampleGame());
+        return gameId;
+    }
+
+    public Game getGame(String gameId) {
+        Game game = games.get(gameId);
+
+        if (game == null) {
+            throw new IllegalArgumentException("Game not found");
+        }
+
+        return game;
     }
 }
