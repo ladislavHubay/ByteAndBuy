965949ae8f6d4bc4e1db50bbb7a184dcefd9d902
ladislavHubay <hubayladislav7@gmail.com>
2026-04-27 14:01:24 +0200

    GameEngine - doplnene komentare

--- src/main/java/org/hubay/byteandbuy/service/GameEngine.java
+++ src/main/java/org/hubay/byteandbuy/service/GameEngine.java
@@ -36,37 +36,20 @@ public class GameEngine {
         GameEventCollector collector = createCollector(game);
 
         if (game.isFinished()) {
-            collector.add("Hra je ukoncena");
             return createFinishedResponse(collector);
         }
 
         Player player = game.getCurrentPlayer();
         TurnResponse response = createRollResponseFor(player);
 
-        collector.add(player.getName() + " je na pozicii " +
-                game.getCurrentTile(player).getName() +
-                ", ma: " + player.getMoney());
+        logRollStart(game, player, collector);
+        int dice = performRoll(game, response, collector);
 
-        int dice = rollDice();
-        game.setDice(dice);
-        response.setDice(dice);
-
-        collector.add("Hodil si: " + dice);
-
-        if (jailService.handleJailTurn(game, player, dice)) {
-            completeAction(game, player, response, collector);
+        if (handleJailRoll(game, player, dice, response, collector)) {
             return response;
         }
 
-        movementService.movePlayer(game, player, dice);
-
-        response.setToPosition(player.getPosition());
-        response.setTileName(game.getCurrentTile(player).getName());
-
-        collector.add(player.getName() + " sa posunul na " + game.getCurrentTile(player).getName());
-
-        tileActionService.resolveTileEffects(game, player);
-        response.setMoney(player.getMoney());
+        resolveRollMovement(game, player, dice, response, collector);
         completeAction(game, player, response, collector);
 
         return response;
@@ -84,10 +67,8 @@ public class GameEngine {
      */
     public TurnResponse buyProperty(Game game) {
         GameEventCollector collector = createCollector(game);
-
         Player player = game.getCurrentPlayer();
         Tile tile = game.getCurrentTile(player);
-
         TurnResponse response = createResponseFor(player);
         response.setTileName(tile.getName());
 
@@ -105,7 +86,6 @@ public class GameEngine {
      */
     public TurnResponse skipPurchase(Game game) {
         GameEventCollector collector = createCollector(game);
-
         Player player = game.getCurrentPlayer();
 
         if (!game.isWaitingForBuy()) {
@@ -138,6 +118,9 @@ public class GameEngine {
         }
     }
 
+    /**
+     * Spracuje tahanie karty ak hrac stoji na policku kde sa ma potiahnut karta.
+     */
     public TurnResponse drawCard(Game game) {
         Player player = game.getCurrentPlayer();
 
@@ -147,24 +130,32 @@ public class GameEngine {
         TurnResponse response = createResponseFor(player);
 
         drawAndResolveCardEffects(game, player);
-
         completeAction(game, player, response, collector);
 
         return response;
     }
 
+    /**
+     * Vytvori objekt GameEventCollector.
+     */
     private GameEventCollector createCollector(Game game) {
         GameEventCollector collector = new GameEventCollector();
         game.setEventCollector(collector);
         return collector;
     }
 
+    /**
+     * Vytvori objekt TurnResponse a ulozi do neho aktualneho hraca pre ukladanie informacii pre frontend.
+     */
     private TurnResponse createResponseFor(Player player) {
         TurnResponse response = new TurnResponse();
         response.setCurrentPlayer(player.getName());
         return response;
     }
 
+    /**
+     * Do objektu TurnResponse a ulozi poziciu a aktualny stav financii hraca.
+     */
     private TurnResponse createRollResponseFor(Player player) {
         TurnResponse response = createResponseFor(player);
         response.setFromPosition(player.getPosition());
@@ -172,18 +163,63 @@ public class GameEngine {
         return response;
     }
 
+    /**
+     * Vytvori odpoved pre frontend v pripade ukoncenia hry.
+     */
     private TurnResponse createFinishedResponse(GameEventCollector collector) {
         TurnResponse response = new TurnResponse();
+        collector.add("Hra je ukoncena");
         response.setEvents(collector.getEvents());
         return response;
     }
 
+    /**
+     * Do objektu GameEventCollector ulozi pre info startovaciu poziciu hraca,
+     * meno hraca a sumu z akou hrac zacina konkretny tah.
+     */
+    private void logRollStart(Game game, Player player, GameEventCollector collector) {
+        collector.add(player.getName() + " je na pozicii " +
+                game.getCurrentTile(player).getName() +
+                ", ma: " + player.getMoney());
+    }
+
+    /**
+     * Vykona hod kockou a ulozi pre potreby frontendu.
+     */
+    private int performRoll(Game game, TurnResponse response, GameEventCollector collector) {
+        int dice = rollDice();
+        game.setDice(dice);
+        response.setDice(dice);
+        collector.add("Hodil si: " + dice);
+        return dice;
+    }
+
+    /**
+     * Spracuje tah hraca vo vazani.
+     * Ak bol tah ukonceny vo vazani vrati true.
+     */
+    private boolean handleJailRoll(Game game, Player player, int dice, TurnResponse response, GameEventCollector collector) {
+        if (!jailService.handleJailTurn(game, player, dice)) {
+            return false;
+        }
+
+        completeAction(game, player, response, collector);
+        return true;
+    }
+
+    /**
+     * Skontroluje ci hrac caka na tahanie karty.
+     */
     private void ensureWaitingForCard(Game game) {
         if (!game.isWaitingForCard()) {
             throw new IllegalStateException("No decision expected");
         }
     }
 
+    /**
+     * Hrac potiahne kartu a vykona efekt karty.
+     * Ak karta zmenila poziciu hraca, vyhodnoti sa dalsie policko.
+     */
     private void drawAndResolveCardEffects(Game game, Player player) {
         int oldPosition = player.getPosition();
         tileActionService.drawCard(game, player);
@@ -193,11 +229,29 @@ public class GameEngine {
         }
     }
 
+    /**
+     * Posunie hraca a vyhodnoti efekt policka na ktorom hrac po posunuti stoji.
+     */
+    private void resolveRollMovement(Game game, Player player, int dice, TurnResponse response, GameEventCollector collector) {
+        movementService.movePlayer(game, player, dice);
+        response.setToPosition(player.getPosition());
+        response.setTileName(game.getCurrentTile(player).getName());
+        collector.add(player.getName() + " sa posunul na " + game.getCurrentTile(player).getName());
+        tileActionService.resolveTileEffects(game, player);
+        response.setMoney(player.getMoney());
+    }
+
+    /**
+     * Ukonci tah hraca a pripravy odpoved pre frontend.
+     */
     private void completeAction(Game game, Player player, TurnResponse response, GameEventCollector collector) {
         turnService.finishTurn(game, player);
         finalizeResponse(response, game, collector);
     }
 
+    /**
+     * Z finalizuje zoznam udalosti pre odpoved pre frontend.
+     */
     private void finalizeResponse(TurnResponse response, Game game, GameEventCollector collector) {
         response.setNextPlayer(game.getCurrentPlayer().getName());
         response.setEvents(collector.getEvents());
